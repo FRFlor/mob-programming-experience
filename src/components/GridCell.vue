08b5ff9869a87a6ac96c3df0e71b1ce7edbb07b2
Felipe Flor <f.flor@vehikl.com>
2019-04-17 13:26:47 -0400

    Implement Cell

---
+++ src/components/GridCell.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="cell" :class="content">
+        <img v-if="content === CellContent.Station" src="https://res.cloudinary.com/felipeflor/image/upload/v1555262973/laptop.svg" alt="station">
+        <img v-else-if="content === CellContent.Worker" src="https://res.cloudinary.com/felipeflor/image/upload/v1555262973/man-user.svg" alt="worker">
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue, Prop} from 'vue-property-decorator';
+
+    export enum CellContent {
+        Nothing = '',
+        Worker = 'worker',
+        Station = 'station',
+    }
+
+    @Component
+    export default class GridHandler extends Vue {
+        @Prop({default: () => CellContent.Nothing}) content!: CellContent;
+
+        protected CellContent = CellContent;
+    }
+</script>
+
+<style lang="scss" scoped>
+    .cell {
+        background-color: hsl(203, 92%, 90%);
+        outline: hsl(0, 0%, 20%) 1px solid;
+        height: 50px;
+        width: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        img {
+            max-width: 100%;
+            height: 65%;
+        }
+
+        &.worker {
+            &:hover {
+                cursor: pointer;
+                background-color: hsl(30, 92%, 60%);
+            }
+        }
+    }
+</style>
--- src/components/GridHandler.vue
+++ src/components/GridHandler.vue
@@ -1,21 +1,103 @@
 <template>
     <div class="grid-handler">
         <div class="grid">
-            <div class="cell"
-                 v-for="i in gridSize**2"
-                 :key="i">
-                {{i}}
-            </div>
+            <!--First Row-->
+            <grid-cell/>
+            <grid-cell :content="CellContent.Station"/>
+            <grid-cell/>
+            <grid-cell :content="CellContent.Station"/>
+            <grid-cell/>
+            <grid-cell :content="CellContent.Station"/>
+            <grid-cell/>
+            <grid-cell :content="CellContent.Station"/>
+            <grid-cell/>
+            <grid-cell/>
+
+            <!--Second Row-->
+            <grid-cell/>
+            <grid-cell :content="CellContent.Worker"/>
+            <grid-cell/>
+            <grid-cell :content="CellContent.Worker"/>
+            <grid-cell/>
+            <grid-cell :content="CellContent.Worker"/>
+            <grid-cell/>
+            <grid-cell :content="CellContent.Worker"/>
+            <grid-cell/>
+            <grid-cell/>
+
+            <!--Third Row-->
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell :content="CellContent.Station"/>
+            <grid-cell :content="CellContent.Worker"/>
+
+            <!--Fourth Row-->
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+
+            <!--Fifth Row-->
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell/>
+            <grid-cell :content="CellContent.Station"/>
+            <grid-cell :content="CellContent.Worker"/>
+
+            <!--Seventh Row-->
+            <grid-cell/>
+            <grid-cell :content="CellContent.Worker"/>
+            <grid-cell/>
+            <grid-cell :content="CellContent.Worker"/>
+            <grid-cell/>
+            <grid-cell :content="CellContent.Worker"/>
+            <grid-cell/>
+            <grid-cell :content="CellContent.Worker"/>
+            <grid-cell/>
+            <grid-cell/>
+
+            <!--Eighth Row-->
+            <grid-cell/>
+            <grid-cell :content="CellContent.Station"/>
+            <grid-cell/>
+            <grid-cell :content="CellContent.Station"/>
+            <grid-cell/>
+            <grid-cell :content="CellContent.Station"/>
+            <grid-cell/>
+            <grid-cell :content="CellContent.Station"/>
+            <grid-cell/>
+            <grid-cell/>
         </div>
     </div>
 </template>
 
 <script lang="ts">
     import {Component, Vue, Watch} from 'vue-property-decorator';
-
-    @Component
+    import GridCell, {CellContent} from '@/components/GridCell.vue';
+    @Component({
+        components: {GridCell}
+    })
     export default class GridHandler extends Vue {
-        protected gridSize: number = 5;
+        protected gridSize: number = 10;
+        protected isDraggingOver: boolean[] = [];
+        protected CellContent = CellContent;
 
         private created(): void {
             document.documentElement.style.setProperty('--grid-size', `${this.gridSize}`);
@@ -32,15 +114,5 @@
     .grid {
         display: grid;
         grid-template-columns: repeat(var(--grid-size), 50px);
-    }
-
-    .cell {
-        background-color: hsl(203, 92%, 90%);
-        outline: hsl(0, 0%, 20%) 1px solid;
-        height: 50px;
-        width: 50px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
     }
 </style>
