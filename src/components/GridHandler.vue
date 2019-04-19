67b0de8bb3b03edf54dd402d8bd97d00c5bf21b1
Felipe Flor <f.flor@vehikl.com>
2019-04-19 14:19:50 -0400

    Implement context menu for workstations to allow moving all workers at once

--- src/components/GridHandler.vue
+++ src/components/GridHandler.vue
@@ -2,79 +2,59 @@
     <div class="grid-handler">
         <div class="grid">
             <!-- Top Stations -->
-            <work-station class="station" :id="0" @drag-drop="onDragDrop"
-                          :number-of-workers="workstationWorkers[0]"
-                          @decrement-input="$set(workstationInputs, 0, workstationInputs[0] - 1)"
-                          @increment-output="$set(workstationOutputs, 0, workstationOutputs[0] + 1)"
-                          :output="workstationOutputs[0]"
-                          :input="workstationInputs[0]"/>
-            <work-station class="station" :id="1" @drag-drop="onDragDrop"
-                          :number-of-workers="workstationWorkers[1]"
-                          @decrement-input="$set(workstationInputs, 1, workstationInputs[1] - 1)"
-                          @increment-output="$set(workstationOutputs, 1, workstationOutputs[1] + 1)"
-                          :output="workstationOutputs[1]"
-                          :input="workstationInputs[1]"/>
-            <work-station class="station" :id="2" @drag-drop="onDragDrop"
-                          :number-of-workers="workstationWorkers[2]"
-                          @decrement-input="$set(workstationInputs, 2, workstationInputs[2] - 1)"
-                          @increment-output="$set(workstationOutputs, 2, workstationOutputs[2] + 1)"
-                          :output="workstationOutputs[2]"
-                          :input="workstationInputs[2]"/>
-            <work-station class="station" :id="3" @drag-drop="onDragDrop"
-                          :number-of-workers="workstationWorkers[3]"
-                          @decrement-input="$set(workstationInputs, 3, workstationInputs[3] - 1)"
-                          @increment-output="$set(workstationOutputs, 3, workstationOutputs[3] + 1)"
-                          :output="workstationOutputs[3]"
-                          :input="workstationInputs[3]"/>
+            <div class="station-wrapper" @contextmenu.prevent="openContextForStation($event, i)" v-for="i in [0, 1, 2, 3]">
+                <work-station :id="i" @drag-drop="onDragDrop"
+                              :number-of-workers="workstationWorkers[i]"
+                              @decrement-input="$set(workstationInputs, i, workstationInputs[i] - 1)"
+                              @increment-output="$set(workstationOutputs, i, workstationOutputs[i] + 1)"
+                              :output="workstationOutputs[i]"
+                              :input="workstationInputs[i]"/>
+            </div>
 
             <div class="empty-cell">
                 <button @click="workTheDay">Work!</button>
             </div>
             <div class="empty-cell"></div>
             <div class="empty-cell"></div>
-            <work-station class="station" :id="4" @drag-drop="onDragDrop"
-                          :number-of-workers="workstationWorkers[4]"
-                          @decrement-input="$set(workstationInputs, 4, workstationInputs[4] - 1)"
-                          @increment-output="$set(workstationOutputs, 4, workstationOutputs[4] + 1)"
-                          :output="workstationOutputs[4]"
-                          :input="workstationInputs[4]"/>
+
+
+            <div class="station-wrapper" @contextmenu.prevent="openContextForStation($event, 4)">
+                <work-station :id="4" @drag-drop="onDragDrop"
+                              :number-of-workers="workstationWorkers[4]"
+                              @decrement-input="$set(workstationInputs, 4, workstationInputs[4] - 1)"
+                              @increment-output="$set(workstationOutputs, 4, workstationOutputs[4] + 1)"
+                              :output="workstationOutputs[4]"
+                              :input="workstationInputs[4]"/>
+            </div>
 
             <div class="empty-cell"></div>
             <div class="empty-cell"></div>
             <div class="empty-cell"></div>
-            <work-station class="station" :id="5" @drag-drop="onDragDrop"
-                          :number-of-workers="workstationWorkers[5]"
-                          @decrement-input="$set(workstationInputs, 5, workstationInputs[5] - 1)"
-                          @increment-output="$set(workstationOutputs, 5, workstationOutputs[5] + 1)"
-                          :output="workstationOutputs[5]"
-                          :input="workstationInputs[5]"/>
+            <div class="station-wrapper" @contextmenu.prevent="openContextForStation($event, 5)">
+                <work-station :id="5" @drag-drop="onDragDrop"
+                              :number-of-workers="workstationWorkers[5]"
+                              @decrement-input="$set(workstationInputs, 5, workstationInputs[5] - 1)"
+                              @increment-output="$set(workstationOutputs, 5, workstationOutputs[5] + 1)"
+                              :output="workstationOutputs[5]"
+                              :input="workstationInputs[5]"/>
+            </div>
 
             <!-- Bottom Stations -->
-            <work-station class="station" :id="9" @drag-drop="onDragDrop"
-                          :number-of-workers="workstationWorkers[9]"
-                          @decrement-input="$set(workstationInputs, 9, workstationInputs[9] - 1)"
-                          @increment-output="$set(workstationOutputs, 9, workstationOutputs[9] + 1)"
-                          :output="workstationOutputs[9]"
-                          :input="workstationInputs[9]"/>
-            <work-station class="station" :id="8" @drag-drop="onDragDrop"
-                          :number-of-workers="workstationWorkers[8]"
-                          @decrement-input="$set(workstationInputs, 8, workstationInputs[8] - 1)"
-                          @increment-output="$set(workstationOutputs, 8, workstationOutputs[8] + 1)"
-                          :output="workstationOutputs[8]"
-                          :input="workstationInputs[8]"/>
-            <work-station class="station" :id="7" @drag-drop="onDragDrop"
-                          :number-of-workers="workstationWorkers[7]"
-                          @decrement-input="$set(workstationInputs, 7, workstationInputs[7] - 1)"
-                          @increment-output="$set(workstationOutputs, 7, workstationOutputs[7] + 1)"
-                          :output="workstationOutputs[7]"
-                          :input="workstationInputs[7]"/>
-            <work-station class="station" :id="6" @drag-drop="onDragDrop"
-                          :number-of-workers="workstationWorkers[6]"
-                          @decrement-input="$set(workstationInputs, 6, workstationInputs[6] - 1)"
-                          @increment-output="$set(workstationOutputs, 6, workstationOutputs[6] + 1)"
-                          :output="workstationOutputs[6]"
-                          :input="workstationInputs[6]"/>
+            <div class="station-wrapper" @contextmenu.prevent="openContextForStation($event, i)" v-for="i in [9, 8, 7, 6]">
+                <work-station :id="i" @drag-drop="onDragDrop"
+                              :number-of-workers="workstationWorkers[i]"
+                              @decrement-input="$set(workstationInputs, i, workstationInputs[i] - 1)"
+                              @increment-output="$set(workstationOutputs, i, workstationOutputs[i] + 1)"
+                              :output="workstationOutputs[i]"
+                              :input="workstationInputs[i]"/>
+            </div>
         </div>
+
+        <vue-context ref="menu">
+            <ul class="context-menu">
+                <li @click="moveAllWorkersToContextStation">Move all workers</li>
+            </ul>
+        </vue-context>
     </div>
 </template>
 
@@ -82,10 +62,12 @@
 <script lang="ts">
     import {Component, Vue, Watch} from 'vue-property-decorator';
     import WorkStation from '@/components/WorkStation.vue';
+    import { VueContext } from 'vue-context';
 
-    @Component({components: {WorkStation}})
+    @Component({components: {WorkStation, VueContext}})
     export default class GridHandler extends Vue {
         protected columns: number = 4;
+        protected contextStation: number = 0;
         protected numberOfWorkstations: number = 10;
         protected workstationWorkers: number[] = [];
         protected workstationInputs: number[] = [];
@@ -121,6 +103,17 @@
         protected workTheDay(): void {
             this.workstations.forEach((workstation: any) => workstation.rollDice());
         }
+
+        protected openContextForStation(event: any, stationId: number): void {
+            this.contextStation = stationId;
+            // @ts-ignore;
+            return this.$refs.menu.open(event);
+        }
+
+        protected moveAllWorkersToContextStation() {
+            this.workstationWorkers = Array.from({length: this.numberOfWorkstations}, (_: any) => 0);
+            Vue.set(this.workstationWorkers, this.contextStation, this.numberOfWorkstations);
+        }
     }
 </script>
 
@@ -136,7 +129,7 @@
         transform-origin: 0 0;
         transform: scale(var(--scale));
 
-        .station {
+        .station-wrapper {
             z-index: 2;
         }
 
