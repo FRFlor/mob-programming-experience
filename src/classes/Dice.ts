a020047cdbe7b69065adb209ef3b42f6e6a4be87
Felipe Flor <f.flor@vehikl.com>
2019-04-19 22:02:05 -0400

    Implement Dice Class

---
+++ src/classes/Dice.ts
@@ -0,0 +1,19 @@
+export default class Dice {
+    private static MIN: number = 1;
+    private static MAX: number = 6;
+    private static randomBetween(min: number, max: number): number {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    private faceValue: number = 0;
+
+    public roll(): number {
+        const lastRoll: number = this.faceValue;
+
+        do {
+            this.faceValue = Dice.randomBetween(Dice.MIN, Dice.MAX);
+        } while (lastRoll === this.faceValue);
+
+        return this.faceValue;
+    }
+}
---
+++ tests/unit/dice.spec.ts
@@ -0,0 +1,27 @@
+import Dice from '@/classes/Dice';
+
+const TEST_LOOP_COUNT: number = 100;
+const D6_MIN_VALUE: number = 1;
+const D6_MAX_VALUE: number = 6;
+
+describe('Dice', () => {
+    const dice: Dice = new Dice();
+
+    it('generates only random values between 1 and 6', () => {
+        for (let i = 0; i < TEST_LOOP_COUNT; i++) {
+            const result: number = dice.roll();
+            expect(result).toBeLessThanOrEqual(D6_MAX_VALUE);
+            expect(result).toBeGreaterThanOrEqual(D6_MIN_VALUE);
+        }
+    });
+
+    it('does not repeat the roll value between rolls', () => {
+        let previousResult: number = 0;
+
+        for (let i = 0; i < TEST_LOOP_COUNT; i++) {
+            const result: number = dice.roll();
+            expect(result).not.toEqual(previousResult);
+            previousResult = result;
+        }
+    });
+});
