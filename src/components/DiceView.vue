de3750f1c8f31df01bd66160787a70a8a92b6533
Felipe Flor <f.flor@vehikl.com>
2019-04-23 07:28:31 -0400

    Give levitation effect to dice

--- src/components/DiceView.vue
+++ src/components/DiceView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="dice-view" :class="dice.isRolling ? 'is-rolling' : ''">
+    <div class="dice-view elevated" :class="dice.isRolling ? 'is-rolling' : ''">
         <img v-for="i in 6"
              v-show="dice.value === i"
              :src="require(`../assets/d${i}.svg`)"
@@ -18,12 +18,19 @@
 </script>
 
 <style lang="scss" scoped>
+    .elevated {
+        box-shadow: 0 3px 5px -1px rgba(0,0,0,0.3),
+        0 5px 8px 0 rgba(0,0,0,0.24),
+        0 1px 14px 0px rgba(0,0,0,0.22);
+    }
+
     .dice-view {
-        background-color: white;
+        background-color: hsla(162, 0%, 90%, 0.85);
         width: 100%;
         height: 100%;
         border-radius: 6px;
         transition: all 400ms ease;
+
         img {
             width: 100%;
             height: 100%;
