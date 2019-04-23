0ad92a9612158fcaa696e58a972c944813e5bfd5
Felipe Flor <f.flor@vehikl.com>
2019-04-22 23:12:04 -0400

    Increase Feng-shui of layout by centralizing app contents

--- src/App.vue
+++ src/App.vue
@@ -18,4 +18,13 @@ export default class App extends Vue {
 </script>
 
 <style lang="scss">
+    body {
+        margin: 0;
+        background-color: hsl(30, 40%, 85%);
+        #app {
+            display: flex;
+            justify-content: center;
+            margin-top: 2rem;
+        }
+    }
 </style>
